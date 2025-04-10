5b96160e1fcda22553764c78d3fb1291435741e2
javendaXgh <javenda@gmail.com>
2025-04-10 06:17:34 -0400

    actualizacion app gapminder y madres

--- apps/app_edadmadres.R
+++ apps/app_edadmadres.R
@@ -1,11 +1,4 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
 library(shiny)
 library(openintro)
@@ -21,10 +14,10 @@ ui <- fluidPage(
         sidebarPanel(
             sliderInput("bins",
                         "Cantidad de barras:",
-                        min = 5,
-                        max = 50,
-                        step = 5,
-                        value = 15)
+                        min = 4,
+                        max = 40,
+                        step = 2,
+                        value = 10)
         ),
 
         # Show a plot of the generated distribution
@@ -40,18 +33,30 @@ server <- function(input, output) {
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
       x    <- babies$age[!is.na(babies$age)]
-        # print(x[1:10])
-        bins <- seq(min(x), 
-                    max(x), 
-                    length.out = input$bins + 1)
+      #   # print(x[1:10])
+      #   bins <- seq(min(x), 
+      #               max(x), 
+      #               length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
         hist(x,
-             breaks = bins, 
+             breaks = input$bins, 
              col = 'darkgray', 
              border = 'white',
              xlab = 'Edad Madre',
              main = 'Histograma of de Edades de Madres')
+        par(new=TRUE)
+        points(mean(x),
+               -.05, 
+               pch = 17, 
+               col = "red",
+               cex = 2)
+        
+        points(median(x),
+               -.05, 
+               pch = 17, 
+               col = "orange",
+               cex = 2)
     })
 }
 
--- apps/app_gapminder.R
+++ apps/app_gapminder.R
@@ -1,8 +1,9 @@
 library(shiny)
 library(plotly)
 library(dplyr)
-
-gap_2007 <- read.csv('datos/gap_2007.csv')
+# https://javenda.shinyapps.io/gapminder2007/
+# gap_2007 <- read.csv('datos/gap_2007.csv')
+gap_2007 <- read.csv('https://raw.githubusercontent.com/javendaXgh/ucveconomiaestadistica1/refs/heads/main/apps/datos/gap_2007.csv')
 # para el conjunto de datos gap_2007 que es un subset de gapminder para el año 2007 se
 # crea una aplicación shiny con las siguientes característica
 # en sidebar hay selector de continente con multiples choices
@@ -29,133 +30,163 @@ ui <- fluidPage(
                   choices = unique(gap_2007$continent), 
                   selected ='Americas', 
                   multiple = TRUE),
-      selectInput("paises", "Selecciona países:", 
+      selectInput("paises", 
+                  "Selecciona países:", 
                   choices = NULL, 
                   selected = NULL, 
                   multiple = TRUE),
       checkboxInput('gr_barra',
                     label= 'Gráfico de GDP per Capita por país',
                     value= FALSE),
+      checkboxInput('mean',
+                    label= 'mostrar promedio (línea roja)',
+                    value= FALSE),
 
-      # sliderInput('median',
-      #             label= 'media',
-      #             
-      #             )
-      
+      checkboxInput('median',
+                  label= 'mostrar mediana (línea anaranjada)',
+                  value= FALSE),
+
       uiOutput("paises_ui")
     ),
     mainPanel(
+      verbatimTextOutput("valores"),
       plotlyOutput("histograma"),
-      plotlyOutput("grafico_pais"),
-      verbatimTextOutput("valores")
+      plotlyOutput("grafico_pais")
     )
   )
 )
 
 server <- function(input, output, session) {
-  # Filtrar los países según el continente seleccionado
-  observe({
-    paises_filtrados <- gap_2007 %>%
+  paises_filtrados <-observe({
+    print(input$continente)
+    t <- gap_2007 %>%
       filter(continent %in% input$continente) %>%
       pull(country) %>%
       unique()
+    print(t)
+    updateSelectInput(session,
+                      "paises",
+                      "Selecciona países:",
+                      choices = t,
+                      selected = t)
     
-    updateSelectInput(session, "paises", "Selecciona países:", 
-                      choices = paises_filtrados, 
-                      selected = paises_filtrados)
   })
+  # updateSelectInput(session,
+  #                   "paises",
+  #                   "Selecciona países:",
+  #                   choices = paises_filtrados(),
+  #                   selected = paises_filtrados())
   
-  # Crear el histograma
-  output$histograma <- renderPlotly({
+  # country_seleccionado <- reactiveVal()
+  
+  data_filtrada <- reactive({
     req(input$paises)
-    
-    data_filtrada <- gap_2007 %>%
-      filter(country %in% input$paises)
-    
-    p <- plot_ly(data_filtrada, 
-                 x = ~gdpPercap, 
-                 type = "histogram", 
-                 name = "Histograma de GDP per Capita",
-                 nbinsx = input$bins) %>%
-      layout(title = "Histograma de GDP per Capita",
-             xaxis = list(title = "GDP per Capita"),
-             yaxis = list(title = "Frecuencia"))
-    
-    # if (input$mean) {
-      p <- p %>% add_lines(x = c( mean(data_filtrada$gdpPercap), 
-                            mean(data_filtrada$gdpPercap)), 
-                     y = c(0, 10), 
-                     name= "promedio",
-                     line = list(color = 'red', 
-                                 dash = 'dash'))
-    # }
-    
-    # if (input$median) {
-      p <- p %>% add_lines(x = c( median(data_filtrada$gdpPercap), 
-                                  median(data_filtrada$gdpPercap)), 
-                           y = c(0, 10), 
-                           name= "mediana",
-                           line = list(color = 'orange',
-                                       dash = 'dash',
-                                       text= "Mediana"))
-    # }
-    
-    p
+    gap_2007 %>%
+      filter(country %in% input$paises)%>%
+      arrange(gdpPercap)%>%
+      mutate(country=factor(country,
+                            levels = country) )%>%
+      arrange(gdpPercap)
   })
-  
-  # Crear gráfico de GDP per Capita por país
-  observe({
-    if (input$gr_barra) {
-      output$grafico_pais <- renderPlotly({
-        req(input$paises)
-        
-        data_filtrada <- gap_2007 %>%
-          filter(country %in% input$paises)%>%
-          arrange((gdpPercap))
-        
-        data_filtrada$country <- factor(data_filtrada$country, 
-                                        levels = data_filtrada$country)
-        
-        plot_ly(data_filtrada, x = ~country, y = ~gdpPercap, type = 'bar', 
-                name = 'GDP per Capita') %>%
-          layout(title = 'GDP per Capita por País',
-                 xaxis = list(title = 'País'),
-                 yaxis = list(title = 'GDP per Capita'))
-      })
-    }
-  })
-  # output$grafico_pais <- renderPlotly({
-  #   req(input$paises)
+  # # Filtrar los países según el continente seleccionado
+  # observe({
+  # 
   #   
-  #   data_filtrada <- gap_2007 %>%
-  #     filter(country %in% input$paises)%>%
-  #     arrange((gdpPercap))
-  #   
-  #   data_filtrada$country <- factor(data_filtrada$country, 
-  #                                     levels = data_filtrada$country)
-  #   
-  #   plot_ly(data_filtrada, x = ~country, y = ~gdpPercap, type = 'bar', 
-  #           name = 'GDP per Capita') %>%
-  #     layout(title = 'GDP per Capita por País',
-  #            xaxis = list(title = 'País'),
-  #            yaxis = list(title = 'GDP per Capita'))
+  # 
+  #   # Crear el histograma
+    output$histograma <- renderPlotly({
+      req(input$paises)
+
+      p <- plot_ly(data_filtrada(),
+                   x = ~gdpPercap,
+                   type = "histogram",
+                   name = "Histograma de GDP per Capita",
+                   nbinsx = input$bins) %>%
+        layout(title = "Histograma de GDP per Capita",
+               xaxis = list(title = "GDP per Capita"),
+               yaxis = list(title = "Frecuencia"))
+
+      if (input$mean) {
+        p <- p %>%
+          add_lines(x = c( mean(data_filtrada()$gdpPercap),
+                           mean(data_filtrada()$gdpPercap)),
+                    y = c(-.5, 10),
+                    name= "promedio",
+                    line = list(color = 'red',
+                                dash = 'dash'))
+      }
+
+      if (input$median) {
+        p <- p %>%
+          add_lines(x = c( median(data_filtrada()$gdpPercap),
+                           median(data_filtrada()$gdpPercap)),
+                    y = c(-.5, 10),
+                    name= "mediana",
+                    line = list(color = 'orange',
+                                dash = 'dash',
+                                text= "Mediana"))
+      }
+
+      p%>%
+        layout(showlegend = FALSE)
+    })
+  #   # Crear gráfico de GDP per Capita por país
+    observe({
+      if (input$gr_barra) {
+        output$grafico_pais <- renderPlotly({
+          req(input$paises)
+
+          g <- plot_ly(data_filtrada(),
+                       x = ~country,
+                       y = ~gdpPercap,
+                       type = 'bar',
+                       name = 'GDP per Capita') %>%
+            layout(title = 'GDP per Capita por País',
+                   xaxis = list(title = 'País'),
+                   yaxis = list(title = 'GDP per Capita'))
+
+          # if (input$mean) {
+          #   print(mean(data_filtrada()$gdpPercap))
+          #   g <- g %>%
+          #     add_lines(x = c( mean(data_filtrada()$gdpPercap),
+          #                      mean(data_filtrada()$gdpPercap)),
+          #               y = c(-.0, 10),
+          #               name= "promedio",
+          #               line = list(color = 'red',
+          #                           dash = 'dash'))
+          # }
+          #
+          # if (input$median) {
+          #   g <- g %>%
+          #     add_bars(x = 'promedio',
+          #               y = c( median(data_filtrada()$gdpPercap)),
+          #               name= "mediana",
+          #               line = list(color = 'orange',
+          #                           # dash = 'dash',
+          #                           text= "Mediana"))
+          # }
+
+          g%>%
+            layout(showlegend = FALSE)
+        })
+      }
+    })
+    # Mostrar valores estadísticos
+    output$valores <- renderPrint({
+      req(input$paises)
+
+      mean_val <- round(mean(data_filtrada()$gdpPercap),0)
+      median_val <- round(median(data_filtrada()$gdpPercap),0)
+      harmonic_mean_val <- 1 / mean(1 / data_filtrada()$gdpPercap)
+      mode_val <- as.numeric(names(sort(table(data_filtrada()$gdpPercap), decreasing = TRUE)[1]))
+
+      paste0("Media: ",
+            mean_val,
+            ". Mediana: ",
+            median_val)
+      # cat("Media armónica:", harmonic_mean_val, "\n")
+      # cat("Moda:", mode_val, "\n")
+    })
   # })
-  # Mostrar valores estadísticos
-  output$valores <- renderPrint({
-    req(input$paises)
-    
-    data_filtrada <- gap_2007 %>%
-      filter(country %in% input$paises)
-    
-    mean_val <- mean(data_filtrada$gdpPercap)
-    median_val <- median(data_filtrada$gdpPercap)
-    harmonic_mean_val <- 1 / mean(1 / data_filtrada$gdpPercap)
-    mode_val <- as.numeric(names(sort(table(data_filtrada$gdpPercap), decreasing = TRUE)[1]))
-    
-    cat("Media:", mean_val, "\n")
-    cat("Mediana:", median_val, "\n")
-    cat("Media armónica:", harmonic_mean_val, "\n")
-    cat("Moda:", mode_val, "\n")
-  })
 }
 shinyApp(ui = ui, server = server)
